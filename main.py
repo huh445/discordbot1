749c93179427318c6c7652132a1bbfc88b95174b
huh445 <127354663+huh445@users.noreply.github.com>
2024-04-30 12:08:32 +1000

    welp

--- main.py
+++ main.py
@@ -6,46 +6,51 @@ import asyncio
 from azure.cognitiveservices.speech import SpeechConfig, SpeechSynthesizer, AudioConfig, ResultReason
 
 
-client = commands.Bot(command_prefix = '!',intents=discord.Intents.all())
+client = commands.Bot(command_prefix = '`',intents=discord.Intents.all())
 
 azure_key = "420cdbf55b834a7ebe07ad1444242e2c"
 azure_region = "australiaeast"
 speech_config = SpeechConfig(subscription=azure_key, region=azure_region)
 audio_config = AudioConfig(filename="output.wav")
 synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-@client.command
+@client.event
 async def join(ctx):
-    voice = ctx.author.channel
-    if voice:
-        channel = voice.name
-        await voice.connect()
-        await ctx.send("Joined the voice channel.")
-    else:
-        await ctx.send("User not in voice channel.")
+        voice_channel = ctx.author.voice.channel
+        if voice_channel:
+            voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            if voice_client:
+                await ctx.channel.send("The bot is already in a voice channel")
+            else:
+                voice_client = await voice_channel.connect()
+        else:
+            await ctx.channel.send("You need to be in a voicechat to use the bot")
+
+
 
 
 
 
 @client.event
 async def on_message(message):
-    if message.channel.name == "fort" and message.author != client.user:
+    if message.content.startswith("!"):
+        if message.channel.name == "fort" and message.author != client.user and message.content.startswith != "`join" and message.content.startswith != "`leave":
         # Convert text to speech
-        result = synthesizer.speak_text_async(message.content).get()
-        if result.reason == ResultReason.SynthesizingAudioCompleted:
+            result = synthesizer.speak_text_async(message.content).get()
+            if result.reason == ResultReason.SynthesizingAudioCompleted:
             # Get the voice channel of the user who sent the message
-            voice_channel = message.author.voice.channel
-            if voice_channel:
+                voice_channel = message.author.voice.channel
+                if voice_channel:
                 # Check if the bot is already in a voice channel
-                voice_client = discord.utils.get(client.voice_clients, guild=message.guild)
-                if voice_client:
+                    voice_client = discord.utils.get(client.voice_clients, guild=message.guild)
+                    if voice_client:
                     # If the bot is already in a voice channel, use the existing voice client
-                    await play_audio(voice_client)
-                else:
+                        await play_audio(voice_client)
+                    else:
                     # If the bot is not in a voice channel, connect to the user's voice channel
-                    voice_client = await voice_channel.connect()
-                    await play_audio(voice_client)
-            else:
-                await message.channel.send("You need to be in a voice channel for me to join.")
+                        voice_client = await voice_channel.connect()
+                        await play_audio(voice_client)
+                else:   
+                    await message.channel.send("You need to be in a voice channel for me to join.")
 
 async def play_audio(voice_client):
     # Play the synthesized speech
